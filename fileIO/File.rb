cdf44fe65801e0659518bb93a8f7aa914765714f
kanishk <kanishky@shriffle.com>
2022-10-11 15:37:29 +0530

    fileIO task to add data to csv file using user input

--- fileIO/File.rb
+++ fileIO/File.rb
@@ -5,11 +5,11 @@ headers = ['Name','Phone','City']
 CSV.open("data.csv", "a+") do |csv|
 csv << headers if csv.count.eql? 0
 print 'Enter name : '
-name_ = gets.chomp
+name = gets.chomp
 print 'Enter Phone : '
 phone = gets.chomp.to_i
 print 'Enter city : '
 city = gets.chomp
-csv << [name_,phone,city]
+csv << [name,phone,city]
 end
 
