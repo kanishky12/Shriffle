1d2f2266f43e8775fdd61d0517f8d8817e1ee30a
kanishk <kanishky@shriffle.com>
2022-10-17 15:33:38 +0530

    banking system

--- banking_system/main.rb
+++ banking_system/main.rb
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
 
 require 'csv'
-require 'byebug'
 
 # code starts here
-
 class Bank
   def initialize(balance = 0)
     @balance = balance
@@ -13,92 +11,115 @@ class Bank
   def check
     puts 'Do you have account in HDFC bank y/n'
     chk = gets.chomp
-    if chk == 'y'
-      user_login
-    else
-      user_signup
-    end
+    (chk == 'y') ? user_login : user_signup
+  end
+
+  def user_input
+    puts 'Enter details to create your account'
+    print 'Enter name : '
+    @name = gets.chomp
+    print 'Enter email: '
+    @email = gets.chomp
+    print 'Enter password: '
+    @password = gets.chomp
   end
 
   def user_signup
-    puts 'Enter details to create your account'
-    headers = %w[Name Email Password]
+    headers = %w[Name Email Password balance]
     CSV.open('bank.csv', 'a+') do |csv|
+      user_input
       csv << headers if csv.count.eql? 0
-      print 'Enter name : '
-      _name = gets.chomp
-      print 'Enter email: '
-      email = gets.chomp
-      print 'Enter password: '
-      password = gets.chomp
-      if _name == '' || email == '' || password == ''
-        puts 'Wrong input please check'
+      if empty_entry? then puts 'Enter correct details'
+      elsif duplicate? then puts 'Duplicate data'
       else
-        csv << [_name, email, password]
-        puts 'Registration Successful'
-        puts 'Restart to login'
+        signup(csv)
       end
     end
   end
 
-  def user_login
-    csv = CSV.foreach('bank.csv', headers: true)
-    print 'Enter Email: '
-    email_access = gets.chomp
-    byebug
-    begin
-      row = csv.detect { |e| e['Email'] == email_access }
-    raise 'No such user' unless row
-      print 'Enter password: '
-      password_access = gets.chomp
-    raise 'Wrong password' unless row['Password'] == password_access
+  def empty_entry
+    @name == '' || @email == '' || @password == ''
+  end
 
-      puts 'user logged in successfully'
+  def signup(csv)
+    csv << [@name, @email, @password, @balance]
+    puts 'Registration Successful'
+    access
+  end
+
+  def duplicate?
+    csv = CSV.read('bank.csv', headers: true)
+    csv.find { |row| row['Email'] == @email }
+  end
+
+  def user_login_input
+    puts '------:Welcome to HDFC bank:------'
+    print 'Enter Email: '
+    @email = gets.chomp
+    print 'Enter Password: '
+    @password = gets.chomp
+  end
+
+  def user_login
+    csv = CSV.read('bank.csv', headers: true)
+    user_login_input
+    if csv.find { |row| row['Email'] == @email && row['Password'] == @password }
+      puts 'login successfully'
+      puts '----------------------------------'
       access
+    else
+      puts '!!!wrong email or password try again'
     end
+  end
+
+  def access_input
+    puts 'Enter 1 to check Account Balance'
+    puts 'Enter 2 for Deposit money'
+    puts 'Enter 3 to withdraw money'
+    puts 'Enter 4 to exit'
+    puts '--------------------------------------'
+    @action = gets.chomp.to_i
   end
 
   def access
-    puts 'Enter 1 for Account Balance'
-    puts 'Enter 2 for Deposit money'
-    puts 'Enter 3 to withdraw money'
-    action = gets.chomp.to_i
-    case action
-    when 1
-      display_balance
-    when 2
-      deposit
-    when 3
-      withdraw
-    else
-      puts 'Try again'
+    loop do
+      access_input
+      case @action
+      when 1 then display_balance
+      when 2 then deposit
+      when 3 then withdraw
+      when 4 then break
+      else puts 'Wrong input Try again'
+      end
     end
   end
-end
 
-def display_balance
-  puts "Balance: #{@balance}."
-end
+  def display_balance
+    puts "Balance: #{@balance}."
+    puts '--------------------------------------'
+  end
 
-def deposit
-  puts 'Enter amount to deposit'
-  deposit = gets.chomp.to_i
-  @balance += deposit
-  puts "Deposited: #{deposit}. Updated balance: #{@balance}."
-end
+  def deposit
+    puts 'Enter amount to deposit'
+    deposit = gets.chomp.to_i
+    @balance += deposit
+    puts "Deposited: #{deposit}. Updated balance: #{@balance}."
+    puts '--------------------------------------'
+  end
 
-def withdraw
-  puts 'Enter amount to withdraw'
-  withdraw = gets.chomp.to_i
-  if withdraw <= @balance
-    @balance -= withdraw
-    puts "Withdraw: #{withdraw}. Updated balance: #{@balance}."
-  else
-    puts "You don't have enough balance: #{@balance}"
+  def withdraw
+    puts 'Enter amount to withdraw'
+    withdraw = gets.chomp.to_i
+    if withdraw <= @balance
+      @balance -= withdraw
+      puts "Withdraw: #{withdraw}. Updated balance: #{@balance}."
+      puts '--------------------------------------'
+    else
+      puts '--------------------------------------'
+      puts "You don't have enough balance: #{@balance}"
+    end
   end
 end
 
 bank = Bank.new
 bank.check
-# bank.duplicate
-# bank.update
